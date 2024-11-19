26322778120c34f67b119a3ea661684da6d26ca6
gorm3531 <mcgermafrodit@gmail.com>
2024-11-19 22:43:01 +0700

    22.42

---
+++ Django_2/task/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import *
+
+
+# Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'posts': posts, 'paginator': paginator,
+               'page_number': page_number, 'page_obj': page_obj}
+    return render(request, 'index.html', {'page_obj': page_obj})
